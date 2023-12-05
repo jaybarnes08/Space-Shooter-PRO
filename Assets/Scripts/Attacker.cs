d133ab052aa4ee3cf6693f0085f7617ec5cd5b1b
WavyGamesJay <juwanbarnes@gmail.com>
2023-12-04 19:41:39 -0500

    Updated Aggressive Enemy

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class Attacker : Enemy
 {
+
     float _distanceToPlayer;
-    [SerializeField] float _ramSpeed = 4f;
 
     // Update is called once per frame
     protected override void CalculateMovement()
     {
-        _distanceToPlayer = Vector3.Distance(transform.position, _player.transform.position);
+        _distanceToPlayer = Vector2.Distance(transform.position, _player.transform.position);
+
         if( _distanceToPlayer < 4.4f) {
             transform.position = Vector2.MoveTowards(transform.position, _player.transform.position, (_speed * 1.5f) * Time.deltaTime);
         }
