c7c51c7199b3fbb8c677b0013f9137c260c73f72
Juwan Barnes <juwanbarnes@Juwans-MacBook-Pro.local>
2023-12-14 20:01:46 -0500

    Enemy Boss Implementation v2.0

--- Assets/Scripts/BossEnemy.cs
+++ Assets/Scripts/BossEnemy.cs
@@ -6,6 +6,11 @@ public class BossEnemy : MonoBehaviour
 {
     [SerializeField] float _speed = 3f;
     [SerializeField] GameObject _enemyLaserPrefab;
+    [SerializeField] GameObject _laserBeam;
+    [SerializeField] GameObject _megaShield;
+    [SerializeField] int _health = 25;
+    [SerializeField] int _shieldHealth = 5;
+    bool _shieldActive = true;
     int attackIndex;
 
     // Start is called before the first frame update
@@ -36,26 +41,26 @@ public class BossEnemy : MonoBehaviour
 
     void CycleAttackRoutine()
     {
+        attackIndex = Random.Range(0, 2);
+        Debug.Log(attackIndex);
+
         switch (attackIndex)
         {
             case 0:
                 StartCoroutine(FireLaserRoutine());
                 break;
+            case 1:
+                StartCoroutine(SideToSideLaserRoutine());
+                break;
         }
 
-        attackIndex = Random.Range(0, 0);
     }
-
-    //void ResetAttackIndex()
-    //{
-    //    attackIndex = Random.Range(0, 0);
-    //}
 
     IEnumerator FireLaserRoutine()
     {
         int shotsFired = 0;
 
-        while(shotsFired <= 5)
+        while(shotsFired < 5)
         {
             GameObject enemylaser = Instantiate(_enemyLaserPrefab, transform.position, Quaternion.identity);
             Laser[] lasers = enemylaser.GetComponentsInChildren<Laser>();
@@ -71,9 +76,86 @@ public class BossEnemy : MonoBehaviour
 
         yield return new WaitForSeconds(3f);
         CycleAttackRoutine();
-        
-
-        
 
     }
+
+    IEnumerator SideToSideLaserRoutine()
+    {
+        float moveRight = 1;
+        bool moving = true;
+        bool returnToCenter = false;
+
+        _laserBeam.gameObject.SetActive(true);
+        yield return new WaitForSeconds(2f);
+
+        while (moving)
+        {
+            Debug.Log(moveRight);
+            transform.Translate(Vector2.right * _speed * moveRight * Time.deltaTime);
+
+            if(transform.position.x >= 8f)
+            {
+                moveRight = -1f;
+            }
+
+            if (transform.position.x <= -8f)
+            {
+                moveRight = 1f;
+                returnToCenter = true;
+            }
+
+            if (transform.position.x >= 0 && returnToCenter)
+            {
+                moveRight = 0;
+                yield return new WaitForSeconds(1f);
+                _laserBeam.gameObject.SetActive(false);
+                moving = false;
+
+            }
+
+            yield return null;
+            
+        }
+
+        yield return new WaitForSeconds(3f);
+        CycleAttackRoutine();
+    }
+
+    IEnumerator ShieldCooldownRoutine()
+    {
+        yield return new WaitForSeconds(8f);
+        _shieldHealth += 5;
+        _megaShield.gameObject.SetActive(true);
+        _shieldActive = true;
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Laser"))
+        { 
+            if (_shieldActive)
+            {
+                _shieldHealth--;
+
+                if(_shieldHealth == 0)
+                {
+                    _megaShield.gameObject.SetActive(false);
+                    _shieldActive = false;
+                }
+                else
+                {
+                    _shieldHealth--;
+
+                    if(_health <= 0)
+                    {
+                        Destroy(this.gameObject);
+                    }
+                }
+            }
+
+            Destroy(other.gameObject);
+        }
+    }
+
 }
